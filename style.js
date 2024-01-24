578c256471abe1252310f192e3b7df73cdf795d9
dalton02 <104688487+dalton02@users.noreply.github.com>
2024-01-23 21:30:13 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -6,7 +6,7 @@ var height = Math.max( body.scrollHeight, body.offsetHeight,
 
 let spaceX = [];
 let spaceY = [];
-let quantidadeStars = 50;
+let quantidadeStars = 200;
 
 var star = [];
 
@@ -14,8 +14,6 @@ calculatePos(spaceX,spaceY);
 
 for(let i=0;i<quantidadeStars;i++){
     star[i] = recreate(height);
-    star[i].style.left = spaceX[i];
-    star[i].style.top = spaceY[i];
 }
 
 
@@ -29,8 +27,8 @@ function loopStar(){
     let x = star[i].style.left;
     let posX = x.replace("px","");
     
-    let randomSpeedY = (Math.random()*3)+1;
-    let randomSpeedX = (Math.random()*8)+4;    
+    let randomSpeedY = (Math.random()*-10)-1;
+    let randomSpeedX = (Math.random()*-1);    
     
     star[i].style.top = (posY - randomSpeedY) + "px";
     star[i].style.left = (posX - randomSpeedX)+"px";
@@ -47,6 +45,12 @@ function loopStar(){
         star[i].style.left = spaceX[i];
         star[i].style.top = spaceY[i];
     }
+    else if(posY>height+100){
+        calculatePos(spaceX,spaceY);
+        star[i].style.left = spaceX[i];
+        star[i].style.top = spaceY[i];
+    }
+        
             
     }
     
@@ -58,11 +62,13 @@ function recreate(height){
 var star = document.createElement("div");
 star.style.width = 7+"px";
 star.style.height = 3.5+"px";
-star.style.backgroundColor= "grey";
+star.style.backgroundColor= "white";
 star.style.position = "fixed";
 star.style.left = "0px";
 star.style.zIndex=-1;
-star.style.top =  height+"px";
+star.style.transform = "rotateZ(90deg)"
+
+star.style.top = (Math.random()*height) +"px";
 document.body.appendChild(star);
 return star;
 }
@@ -70,10 +76,10 @@ return star;
 function calculatePos(spaceX,spaceY){
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceX[i] = (Math.random()*( document.body.clientWidth)) + (document.body.clientWidth-50) + "px";
+    spaceX[i] = (Math.random()*( document.body.clientWidth))+ "px";
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceY[i] = (Math.random()*(height)) +50 + "px";
+    spaceY[i] = (Math.random()*10) + "px";
     
     
 }
