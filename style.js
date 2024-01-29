d5d61cc17ce8a5b62d51948f0377718ac8bde471
Dalton Gomes Lobato <104688487+dalton02@users.noreply.github.com>
2024-01-28 21:16:23 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -13,7 +13,7 @@ var star = [];
 calculatePos(spaceX,spaceY);
 
 for(let i=0;i<quantidadeStars;i++){
-    star[i] = recreate(height);
+    star[i] = solar(height);
 }
 
 
@@ -27,25 +27,23 @@ function loopStar(){
     let x = star[i].style.left;
     let posX = x.replace("px","");
     
-    let randomSpeedY = (Math.random()*-10)-1;
-    let randomSpeedX = (Math.random()*-1);    
+    let r = star[i].style.transform;
+    let r1 = r.replace("rotateZ(","");
+    let r2 = r1.replace("deg)","");
     
-    star[i].style.top = (posY - randomSpeedY) + "px";
-    star[i].style.left = (posX - randomSpeedX)+"px";
-    star[i].style.opacity = 0.8;
+    star[i].style.top = (parseInt(posY) + 1) + "px";
+    star[i].style.left = (parseInt(posX) + 0)+"px";
+    
+    star[i].style.transform = "rotateZ("+(parseInt(r2)+1)+"deg)";
+    let limit = document.body.clientWidth;
+    if(posX>limit){
+        calculatePos(spaceX,spaceY);
+        star[i].style.left = spaceX[i];
+        star[i].style.top = spaceY[i];
+        console.log(spaceX[i]);
+    }
         
-    
-    if(posX<0){
-        calculatePos(spaceX,spaceY);
-        star[i].style.left = spaceX[i];
-        star[i].style.top = spaceY[i];
-    }
-    else if(posY<0){
-        calculatePos(spaceX,spaceY);
-        star[i].style.left = spaceX[i];
-        star[i].style.top = spaceY[i];
-    }
-    else if(posY>height+100){
+    else if(posY>height){
         calculatePos(spaceX,spaceY);
         star[i].style.left = spaceX[i];
         star[i].style.top = spaceY[i];
@@ -58,28 +56,71 @@ function loopStar(){
 }
 window.requestAnimationFrame(loopStar);
 
-function recreate(height){
-var star = document.createElement("div");
-star.style.width = 7+"px";
-star.style.height = 3.5+"px";
-star.style.backgroundColor= "white";
-star.style.position = "fixed";
-star.style.left = "0px";
-star.style.zIndex=-1;
-star.style.transform = "rotateZ(90deg)"
 
+function rain(){
+    
+var star = document.createElement("div");
+let randomT = (Math.random()*1.5)+0.5;
+
+star.style.width = randomT+"px";
+star.style.height = randomT+70+"px";
+    
+star.style.backgroundColor= "aqua";
+star.style.opacity = (Math.random()*1)+0.9;
+star.style.position = "absolute";
+star.style.left = Math.random()*document.body.clientWidth+(-200)+"px";
+star.style.zIndex=-1;
 star.style.top = (Math.random()*height) +"px";
+star.style.transform = "rotateZ(-20deg)";
+    
+document.body.style.position = "relative";
+document.body.style.margin = "0";
 document.body.appendChild(star);
+    
+return star;
+    
+}
+
+function solar(){
+var star = document.createElement("div");
+let randomT = (Math.random()*2.5)+0.5;
+let randomB = (Math.random()*170)+90;
+let choose = (Math.random()*100)+0;
+    
+if(choose>99.4)   {
+star.style.width = randomB+"px";
+star.style.height = randomB+"px";
+}else{
+star.style.width = randomT+"px";
+star.style.height = randomT+"px";
+    
+}
+    
+star.style.backgroundColor= "white";
+star.style.opacity = (Math.random()*1)+0.9;
+star.style.position = "absolute";
+star.style.left = Math.random()*document.body.clientWidth+"px";
+star.style.zIndex=-1;
+star.style.overflow = "hidden";
+star.style.borderRadius = "100%";
+star.style.boxShadow = "0px 0px "+(parseInt(star.style.width)*3)+"px "+(parseInt(star.style.width)/7)+"px rgb(243,211,100)";
+star.style.top = (Math.random()*height) +"px";
+star.style.transform = "rotateZ(90deg)";
+    
+document.body.style.position = "relative";
+document.body.style.margin = "0";
+document.body.appendChild(star);
+    
 return star;
 }
 
 function calculatePos(spaceX,spaceY){
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceX[i] = (Math.random()*( document.body.clientWidth))+ "px";
+    spaceX[i] = (Math.random()*( document.body.clientWidth))+ (-10) + "px";
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceY[i] = (Math.random()*10) + "px";
+    spaceY[i] = (Math.random()*50)+40 + "px";
     
     
 }
