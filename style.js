278d6f2d25be1fec00bf44dde76a027f34acd408
Dalton Gomes Lobato <104688487+dalton02@users.noreply.github.com>
2024-02-03 13:48:36 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -6,16 +6,27 @@ var height = Math.max( body.scrollHeight, body.offsetHeight,
 
 let spaceX = [];
 let spaceY = [];
-let quantidadeStars = 200;
+let quantidadeStars = 50;
+let quantidadeRains = 5;
+let divA = document.getElementById("animation");
+divA.style.height = height+"px";
 
 var star = [];
-
+var speed = [];
+var speedR = [];
+var rain = [];
 calculatePos(spaceX,spaceY);
 
+
 for(let i=0;i<quantidadeStars;i++){
-    star[i] = solar(height);
+    star[i] = solar(i);
+    
 }
 
+for(let i=0;i<quantidadeRains;i++){
+    rain[i] = rainer();
+    speedR[i]= Math.random()*14+10;    
+}
 
 function loopStar(){
     
@@ -31,24 +42,37 @@ function loopStar(){
     let r1 = r.replace("rotateZ(","");
     let r2 = r1.replace("deg)","");
     
-    star[i].style.top = (parseInt(posY) + 1) + "px";
+    star[i].style.top = (parseInt(posY) + speed[i]) + "px";
     star[i].style.left = (parseInt(posX) + 0)+"px";
     
     star[i].style.transform = "rotateZ("+(parseInt(r2)+1)+"deg)";
     let limit = document.body.clientWidth;
-    if(posX>limit){
+    if(posX>limit || posY>height){
         calculatePos(spaceX,spaceY);
-        star[i].style.left = spaceX[i];
-        star[i].style.top = spaceY[i];
-        console.log(spaceX[i]);
+        star[i].style.left = spaceX[i] + "px";
+        star[i].style.top = spaceY[i] + "px";
     }
-        
-    else if(posY>height){
+            
+    }
+    
+    for(let i=0;i<quantidadeRains;i++){    
+    
+    let y = rain[i].style.top;
+    let posY = y.replace("px","");
+    
+    let x = rain[i].style.left;
+    let posX = x.replace("px","");
+    
+    rain[i].style.top = (parseInt(posY) + speedR[i]) + "px";
+    rain[i].style.left = (parseInt(posX) + speedR[i])+"px";
+    
+    let limit = document.body.clientWidth;
+    
+    if(posX>limit || posY>height){
         calculatePos(spaceX,spaceY);
-        star[i].style.left = spaceX[i];
-        star[i].style.top = spaceY[i];
+        rain[i].style.left =  -150 + "px";
+        rain[i].style.top = Math.random()*height + "px";
     }
-        
             
     }
     
@@ -57,59 +81,67 @@ function loopStar(){
 window.requestAnimationFrame(loopStar);
 
 
-function rain(){
+function rainer(){
     
 var star = document.createElement("div");
-let randomT = (Math.random()*1.5)+0.5;
+let randomT = (Math.random()*1)+0.5;
 
 star.style.width = randomT+"px";
 star.style.height = randomT+70+"px";
     
-star.style.backgroundColor= "aqua";
+star.style.backgroundColor= "rgb(245,245,255)";
 star.style.opacity = (Math.random()*1)+0.9;
 star.style.position = "absolute";
 star.style.left = Math.random()*document.body.clientWidth+(-200)+"px";
 star.style.zIndex=-1;
+star.style.overflow="hidden";
 star.style.top = (Math.random()*height) +"px";
-star.style.transform = "rotateZ(-20deg)";
-    
+star.style.transform = "rotateZ(-43deg)";
+//star.style.borderRadius = "100%";
+//star.style.boxShadow = "0px 0px "+(parseInt(star.style.width)*3)+"px "+(parseInt(star.style.width)/7)+"px "+star.style.backgroundColor+"";
+star.style.filter = "drop-shadow(0px -30px 1px "+star.style.backgroundColor+")";
 document.body.style.position = "relative";
 document.body.style.margin = "0";
-document.body.appendChild(star);
+divA.appendChild(star);
     
 return star;
     
 }
 
-function solar(){
+function solar(i){
 var star = document.createElement("div");
-let randomT = (Math.random()*2.5)+0.5;
-let randomB = (Math.random()*170)+90;
+let randomT = (Math.random()*5.5)+0.5;
+let randomB = (Math.random()*300)+170;
 let choose = (Math.random()*100)+0;
     
-if(choose>99.4)   {
+if(choose>99)   {
 star.style.width = randomB+"px";
 star.style.height = randomB+"px";
+star.style.backgroundColor= "rgb(233,246,255)";
+speed[i]=1;
 }else{
+
+if(randomT<3)
+    speed[i]=1;
+else
+    speed[i]=2;
 star.style.width = randomT+"px";
 star.style.height = randomT+"px";
-    
+
+star.style.backgroundColor= "white";
 }
     
-star.style.backgroundColor= "white";
 star.style.opacity = (Math.random()*1)+0.9;
 star.style.position = "absolute";
 star.style.left = Math.random()*document.body.clientWidth+"px";
 star.style.zIndex=-1;
 star.style.overflow = "hidden";
 star.style.borderRadius = "100%";
-star.style.boxShadow = "0px 0px "+(parseInt(star.style.width)*3)+"px "+(parseInt(star.style.width)/7)+"px rgb(243,211,100)";
+star.style.boxShadow = "0px 0px "+(parseInt(star.style.width)*3)+"px "+(parseInt(star.style.width)/7)+"px "+star.style.backgroundColor+"";
 star.style.top = (Math.random()*height) +"px";
 star.style.transform = "rotateZ(90deg)";
-    
-document.body.style.position = "relative";
-document.body.style.margin = "0";
-document.body.appendChild(star);
+
+divA.appendChild(star);
     
 return star;
 }
@@ -117,10 +149,10 @@ return star;
 function calculatePos(spaceX,spaceY){
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceX[i] = (Math.random()*( document.body.clientWidth))+ (-10) + "px";
+    spaceX[i] = (Math.random()*( document.body.clientWidth))+ (-10);
 
 for(let i=0;i<quantidadeStars;i++)
-    spaceY[i] = (Math.random()*50)+40 + "px";
+    spaceY[i] = (Math.random()*-10)+0;
     
     
 }
