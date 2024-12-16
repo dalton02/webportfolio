2c49084a69a85a185279b88550f1d19dac4c673c
daltonPinaculo <dalton@pinaculodigital.com.br>
2024-12-16 10:25:11 -0300

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,30 +4,30 @@
   import Mouse from "$components/elements/window/Mouse.svelte";
   import PopMouse from "$components/sections/pops/PopMouse.svelte";
   import mouseState from "$lib/localData/mouse.svelte";
-
+  import { base } from "$app/paths";
    
 </script>
 
 <svelte:head>
   <title>Portfolio</title>
-  <link rel="manifest" href="/manifest.json">
+  <link rel="manifest" href="{base}/manifest.json">
   <!-- Meta tags e ícones específicos para dispositivos Apple -->
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="Pináculo Digital">
-  <link rel="apple-touch-icon" href="/images/icon-192x192.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/images/icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/images/icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="192x192" href="/images/icon-192x192.png">
-  <link rel="apple-touch-icon" sizes="512x512" href="/images/icon-512x512.png">
+  <link rel="apple-touch-icon" href="{base}/images/icon-192x192.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="{base}/images/icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="{base}/images/icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="192x192" href="{base}/images/icon-192x192.png">
+  <link rel="apple-touch-icon" sizes="512x512" href="{base}/images/icon-512x512.png">
   <!-- Splash screens para diferentes dispositivos Apple -->
-  <link href="/images/screenshot-320x480.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
-  <link href="/images/screenshot-480x320.png" media="(device-width: 480px) and (device-height: 320px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
-  <link href="/images/screenshot-720x1280.png" media="(device-width: 720px) and (device-height: 1280px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
-  <link href="/images/screenshot-1280x720.png" media="(device-width: 1280px) and (device-height: 720px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
-  <link href="/images/screenshot-1080x1920.png" media="(device-width: 1080px) and (device-height: 1920px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
-  <link href="/images/screenshot-1920x1080.png" media="(device-width: 1920px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+  <link href="{base}/images/screenshot-320x480.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+  <link href="{base}/images/screenshot-480x320.png" media="(device-width: 480px) and (device-height: 320px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+  <link href="{base}/images/screenshot-720x1280.png" media="(device-width: 720px) and (device-height: 1280px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+  <link href="{base}/images/screenshot-1280x720.png" media="(device-width: 1280px) and (device-height: 720px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+  <link href="{base}/images/screenshot-1080x1920.png" media="(device-width: 1080px) and (device-height: 1920px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+
 </svelte:head>
 
 
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			fallback: "404.html",
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/webportfolio' : '',
 		},
 		prerender: {
 			entries: ['*']
