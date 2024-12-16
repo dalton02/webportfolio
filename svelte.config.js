055c3bf912cf4c316aeb56ad56f6f59ed7276184
daltonPinaculo <dalton@pinaculodigital.com.br>
2024-12-16 10:13:22 -0300

    .

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,7 @@ const config = {
 	kit: {
 		// adapter: adapter(),
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: "404.html",
-			precompress: false,
-			strict: true
-
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
