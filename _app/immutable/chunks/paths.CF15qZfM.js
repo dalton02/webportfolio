3eef2005f32072bf320c8708e326d590324b0cc0
daltonPinaculo <dalton@pinaculodigital.com.br>
2024-12-16 10:29:59 -0300

    Updates

---
+++ _app/immutable/chunks/paths.CF15qZfM.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1kx43mf)==null?void 0:s.base)??"/webportfolio";var e;const a=((e=globalThis.__sveltekit_1kx43mf)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/nodes/1.B85ju2nr.js
@@ -0,0 +1,1 @@
+import{a,t as o}from"../chunks/disclose-version.bbfCy3wq.js";var t=o('<div class="flex">Se perdeu</div>');function d(e){var r=t();a(e,r)}export{d as component};
