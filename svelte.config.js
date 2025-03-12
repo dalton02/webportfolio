1ed6ea28da94a13783d89e3162ba97a6461fd519
daltonPinaculo <dalton@pinaculodigital.com.br>
2025-03-12 20:45:06 -0300

    fix -> warn deploy errors

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,9 @@ const dev =  process.argv.includes('dev')
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		prerender:{
-			handleHttpError:"warn",
-			
 		},
 		alias: {
 			$components: resolve('src/components'),
