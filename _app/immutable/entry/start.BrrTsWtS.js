5b2190a364b52f9cd8f019381e0188b3ba6b82a5
daltonPinaculo <dalton@pinaculodigital.com.br>
2026-03-22 15:21:09 -0300

    Updates

--- _app/immutable/entry/start.BrrTsWtS.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BLyMZf9R.js";export{o as load_css,r as start};
--- _app/immutable/nodes/2.DuppiNML.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import{p,a as r}from"../chunks/DBocTR6b.js";import{g as t}from"../chunks/BLyMZf9R.js";function s(a,o){p(o,!0),t("/home"),r()}export{s as component};
